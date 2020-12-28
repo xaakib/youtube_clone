878d32d997a0966d729700a391cd65e6b3cbb225
Xaakib <64243974+xaakib@users.noreply.github.com>
2020-12-28 15:54:02 +0600

    first ui done

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:line_icons/line_icons.dart';
+import 'package:youtube_clone/jason/home_videos.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -11,7 +11,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF1b1c1e),
-      body: getBody(),
+      body: SafeArea(child: getBody()),
     );
   }
 
@@ -76,9 +76,110 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ),
                 ],
-              )
+              ),
             ],
           ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 20, right: 20),
+          child: Text(
+            "Recommended",
+            style: TextStyle(
+                color: Colors.white, fontSize: 17, fontWeight: FontWeight.bold),
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Column(
+          children: List.generate(home_video.length, (index) {
+            return Column(children: [
+              Padding(
+                padding: EdgeInsets.only(bottom: 20),
+                child: InkWell(
+                  onTap: () {},
+                  child: Container(
+                    height: 200,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        image: DecorationImage(
+                            image:
+                                NetworkImage(home_video[index]['thumnail_img']),
+                            fit: BoxFit.cover)),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          image: DecorationImage(
+                              image: NetworkImage(
+                                  home_video[index]['profile_img']),
+                              fit: BoxFit.cover)),
+                    ),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Container(
+                      width: size.width - 120,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            home_video[index]['title'],
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500,
+                                height: 1.3),
+                          ),
+                          SizedBox(
+                            height: 2,
+                          ),
+                          Row(
+                            children: <Widget>[
+                              Container(
+                                width: size.width - 120,
+                                child: Text(
+                                  home_video[index]['username'] +
+                                      " - " +
+                                      home_video[index]['day_ago'] +
+                                      " - " +
+                                      home_video[index]['view_count'],
+                                  maxLines: 2,
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 12,
+                                      height: 1.5),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                    Icon(
+                      Icons.more_vert,
+                      color: Colors.white,
+                    )
+                  ],
+                ),
+              ),
+            ]);
+          }),
         )
       ],
     );
